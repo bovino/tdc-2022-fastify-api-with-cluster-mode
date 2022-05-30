f8fe004c8ac5cd83c3d906f04e101de81629513d
bovino <FalaSerio@2016>
2022-05-30 09:56:26 -0300

    removing cluster mode

--- README.md
+++ README.md
@@ -1,8 +1,2 @@
 # tdc-2022-fastify-api-with-cluster-mode
-Sample NodeJS Fastify REST API for TDC 2022 JS talk
-
-Version of the API with cluster mode: <br>
-<https://github.com/bovino/tdc-2022-fastify-api-with-cluster-mode/tree/with-cluster-mode>
-
-Version of the API without cluster mode: <br>
-<https://github.com/bovino/tdc-2022-fastify-api-with-cluster-mode/tree/without-cluster-mode>
+Sample NodeJS Fastify REST API for TDC 2022 JS talk - Version without cluster mode
--- index.js
+++ index.js
@@ -1,22 +1,10 @@
-const cluster = require('cluster');
-const numCPUs = require('os').cpus().length;
 const fastify = require('fastify')();
 const port = 8123;
 
-if (cluster.isMaster) {
-  console.log(`Master ${process.pid} is running`);
-  for (let i = 0; i < numCPUs; i++) {
-    cluster.fork();
-  }
-  cluster.on('exit', worker => {
-    console.log(`Worker ${worker.process.pid} died`);
-  });
-} else {
-  fastify.get('/usuarios', (req, res) => {
-    console.log(' GET REQUEST recebida em ' + new Date().toISOString());
-    res.send('Hello World Rota GET!');
-  });
-  fastify.listen(port, () => {
-    console.log(`Fastify "Hello World" listening on port ${port}, PID: ${process.pid}`);
-  });
-}
+fastify.get('/usuarios', (req, res) => {
+  console.log(' GET REQUEST recebida em ' + new Date().toISOString());
+  res.send('Hello World Rota GET!');
+});
+fastify.listen(port, () => {
+  console.log(`Fastify "Hello World" listening on port ${port}, PID: ${process.pid}`);
+});
